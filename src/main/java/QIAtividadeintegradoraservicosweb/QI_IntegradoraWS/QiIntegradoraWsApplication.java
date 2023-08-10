5573ed310de1adb6ddb0a9e9ba9431464703f498
Daneil_M_R <danroch1913@gmail.com>
2023-08-10 19:14:00 -0300

    Inicio

---
+++ src/main/java/QIAtividadeintegradoraservicosweb/QI_IntegradoraWS/QiIntegradoraWsApplication.java
@@ -0,0 +1,13 @@
+package QIAtividadeintegradoraservicosweb.QI_IntegradoraWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiIntegradoraWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiIntegradoraWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/QIAtividadeintegradoraservicosweb/QI_IntegradoraWS/QiIntegradoraWsApplicationTests.java
@@ -0,0 +1,13 @@
+package QIAtividadeintegradoraservicosweb.QI_IntegradoraWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiIntegradoraWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
