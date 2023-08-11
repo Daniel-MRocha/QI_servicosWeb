47e0c599bb0ce898f5b8570e5fe5661ed2bf0ef0
Daneil_M_R <danroch1913@gmail.com>
2023-08-11 14:52:21 -0300

    DTO criado

---
+++ src/main/java/QIAtividadeintegradoraservicosweb/QI_IntegradoraWS/DTO/Aluno_dto.java
@@ -0,0 +1,68 @@
+package QIAtividadeintegradoraservicosweb.QI_IntegradoraWS.DTO;
+
+public class Aluno_dto {
+    private int id;
+    private String nome;
+    private String email;
+    private int ciclo;
+    private int idade;
+
+    @Deprecated
+    public Aluno_dto(){}
+
+    //construtor com parâmetros
+    public Aluno_dto(int _id, String _nome, String _email, int _ciclo, int _idade){
+        super();
+        id = _id;
+        nome = _nome;
+        email = _email;
+        ciclo = _ciclo;
+        idade = _idade;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getCiclo() {
+        return ciclo;
+    }
+    public int getIdade() {
+        return idade;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    //Método seed, apenas para simulação do Database
+
+    @Override
+    public String toString(){
+        return "Identificação: " + id +
+                "Nome: " + nome +
+                "Email: " + email +
+                "Ciclo: " + ciclo +
+                "Idade: " + idade;
+    }
+
+}
--- src/main/java/QIAtividadeintegradoraservicosweb/QI_IntegradoraWS/QiIntegradoraWsApplication.java
+++ src/main/java/QIAtividadeintegradoraservicosweb/QI_IntegradoraWS/QiIntegradoraWsApplication.java
@@ -1,13 +1,19 @@
 package QIAtividadeintegradoraservicosweb.QI_IntegradoraWS;
 
+import QIAtividadeintegradoraservicosweb.QI_IntegradoraWS.DTO.Aluno_dto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class QiIntegradoraWsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QiIntegradoraWsApplication.class, args);
+		Aluno_dto aluno = new Aluno_dto(1,"daniel","Dani@hotmail.com",3,44);
+		System.out.println(aluno.toString());
 	}
 
 }
