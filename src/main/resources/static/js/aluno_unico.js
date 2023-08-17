e203c17c635d3448edeb3df097be31119bc44df4
Daneil_M_R <danroch1913@gmail.com>
2023-08-17 17:42:22 -0300

    visualizar recurso aluno único OK

--- src/main/resources/static/js/aluno_unico.js
+++ src/main/resources/static/js/aluno_unico.js
@@ -1,9 +1,36 @@
-let url_do_aluno = "http://localhost:8080/alunos/2"
-let aluno
+const bnt_form = document.querySelector("#send")
+
+
+let url_do_aluno;
+    bnt_form.addEventListener("click",async function(e){
+    e.preventDefault()
+    
+    let limpa_registro_aluno = document.querySelector("span")
+    if(limpa_registro_aluno!=null){
+        limpa_registro_aluno.remove();
+    }
+
+    let numero = document.querySelector("#aluno_id")
+    let id_passado =  numero.value
+    url_do_aluno = "http://localhost:8080/alunos/" + id_passado
+    aluno_pos()
+    setTimeout(() => {inseri_aluno(aluno)},750)
+})
+  
 async function aluno_pos(){
-    let aln = await fetch(url_do_aluno)
-    .then(aln => aln.json())
-    .then(aln => aluno = aln)
+   let aln = await fetch(url_do_aluno)
+   .then(aln => aln.json())
+   .then(aln => aluno = aln)
+   .catch(aln => aluno = {id: "--" , nome: "Registro inexistente" , email: "--" , ciclo: "--" , idade: "--"})
 }
 
-aluno = aluno_pos()
+let aluno;
+
+    function inseri_aluno(obj){
+    let apresenta = document.querySelector("#formulario")
+    let insercao = document.createElement("span")
+    apresenta.insertAdjacentElement("afterbegin",insercao)
+        insercao.innerHTML = "ID: " + obj.id + " Nome: " + obj.nome + " Email: " + obj.email + " Ciclo: " + obj.ciclo + " Idade: " + obj.idade 
+}
+
+
